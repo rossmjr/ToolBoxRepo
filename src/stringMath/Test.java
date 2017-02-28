01bd5ac76f1b17bd9e979fe9da8160cb4233f944
MKetchum <MKetchum@student.neumont.edu>
2017-02-28 13:30:42 -0700

    Added unit conversions

--- src/stringMath/Test.java
+++ src/stringMath/Test.java
@@ -1,11 +1,18 @@
 package stringMath;
 
 public class Test {
+	
+	//Have the user input their value as a String with whatever base they want
 
+	/**
+	 * @param
+	 * @return  
+	 */
 	public static void main(String[] args){
 		StringMath math = new StringMath();
-//		System.out.print(math.convertBases(498, 2));
+//		System.out.print(math.convertBases(590, 16));
 //		System.out.print(math.decimalToBinary(0));
-		System.out.print(math.decimalToOctal(98));
+//		System.out.print(math.decimalToOctal(98));
+		System.out.println(math);
 	}
 }
