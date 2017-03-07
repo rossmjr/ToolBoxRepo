88b31327d7194996186b8dc20d27d60d667e9971
MKetchum <MKetchum@student.neumont.edu>
2017-03-07 14:33:09 -0700

    Equations is complete

--- src/stringMath/Test.java
+++ src/stringMath/Test.java
@@ -1,18 +1,13 @@
 package stringMath;
 
 public class Test {
-	
-	//Have the user input their value as a String with whatever base they want
 
-	/**
-	 * @param
-	 * @return  
-	 */
 	public static void main(String[] args){
-//		StringMath math = new StringMath();
+		StringMath math = new StringMath();
 		Equations test = new Equations();
 //		test.equation("( 5 * 4 ) + 3");
-		test.equation("3 * 4 + 2 / ( 1 - 5 ) ^ 2 ^ 3");
+		test.equation("18 - 9 * 4 / ( 3 - 12 ) ^ 5 ^ 2");
+//		System.out.println(test.solve());
 //		System.out.print(math.convertBases(590, 16));
 //		System.out.print(math.decimalToBinary(0));
 //		System.out.print(math.decimalToOctal(98));
