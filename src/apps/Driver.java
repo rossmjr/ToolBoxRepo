09be97c690519538a7b0465dfa075b9db96ac436
Matt Ross <mattross87@gmail.com>
2017-03-09 02:53:44 -0700

    Add CSS sheet to Scene & Undecorated Stage Style
    Want to discuss undecorated stage style in class

--- src/apps/Driver.java
+++ src/apps/Driver.java
@@ -5,16 +5,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Driver extends Application{
 
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+//			primaryStage.initStyle(StageStyle.UNDECORATED);
 			Parent root = FXMLLoader.load(getClass().getResource("SLKDoc.fxml"));
 			
 			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
+			String style = getClass().getResource("main.css").toExternalForm();
+			scene.getStylesheets().addAll(style);
 			primaryStage.setTitle("ToolBox Library Demo");
 			primaryStage.setAlwaysOnTop(true);
 			primaryStage.setResizable(false);
